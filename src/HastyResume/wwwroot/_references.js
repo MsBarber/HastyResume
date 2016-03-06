8c7fbdf28885f310534eedae27b3157d8aba238b
Slick4ceStudios <revon210@hotmail.com>
2016-03-05 23:04:57 -0500

    crosses fingers
    gg

--- src/HastyResume/wwwroot/_references.js
+++ src/HastyResume/wwwroot/_references.js
@@ -3,5 +3,6 @@
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-simple-slider/js/simple-slider.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
